c56c9fc931a0d4d074fb126a8e9e55e576758cce
David Galeano <davidalejangp@gmail.com>
2021-01-04 10:43:56 -0500

    RN Paper
    React Native paper instalado y probado
    (Contiene Warnings)

--- themovieapp/App.js
+++ themovieapp/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
+import { Provider as PaperProvider } from 'react-native-paper'
+import { Button, Card } from 'react-native-paper'
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>HOLAAAAA</Text>
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView>
+        <Text>HOLAAAAA</Text>
+        <Button mode="contained" onPress={() => console.log('Presionado')}>
+          Press me
+        </Button>
+        <Card>
+          <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions>
+        </Card>
+      </SafeAreaView>
+    </PaperProvider>
   )
 }
