b6aaa74a56f64e914984061d06aa89d96040dc9d
David Galeano <davidalejangp@gmail.com>
2021-01-04 12:19:27 -0500

    iconos instalados
    104 fin

--- themovieapp/App.js
+++ themovieapp/App.js
@@ -7,14 +7,14 @@ export default function App() {
   return (
     <PaperProvider>
       <SafeAreaView>
-        <Text>HOLAAAAA</Text>
-        <Button mode="contained" onPress={() => console.log('Presionado')}>
+        <Text>HOLAAAAAs</Text>        
+        <Button icon="camera" mode="contained" onPress={() => console.log('Presionado')}>
           Press me
         </Button>
         <Card>
           <Card.Actions>
             <Button>Cancel</Button>
-            <Button>Ok</Button>
+            <Button  onPress={() => console.log('OKKKk')} >Ok</Button>
           </Card.Actions>
         </Card>
       </SafeAreaView>
