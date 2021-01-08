6dfa9661e6dbfdc0746daa5d2b6d497f66dd3a1e
David Galeano <davidalejangp@gmail.com>
2021-01-08 11:41:37 -0500

    Api configurada
    configurada y extrae datos bien 115

--- themovieapp/src/screens/Home.js
+++ themovieapp/src/screens/Home.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { getNewsMoviesApi } from "../api/movie";
 
 export default function Home() {
+    const [newMovies, setNewMovies] = useState(null);
+
+    useEffect(() => {
+        getNewsMoviesApi().then((response) =>{
+            setNewMovies(response.results);
+        });
+    }, []);
+    console.log(newMovies);
+
     return (
         <View>
             <Text>Estamos en Inicio</Text>
